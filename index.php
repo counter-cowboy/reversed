86db2720a4161b5c483a21f7c1ada64d3fce727a
counter-cowboy <79208562003@ya.ru>
2024-03-11 23:14:03 +0300

    feat: commenting the code

--- index.php
+++ index.php
@@ -2,15 +2,22 @@
 
 function reverseWords($str)
 {
-
+    // разбиваем входящую строку на слова, используя разделитель, описанный в RegExp.
+    // разделитель также попадает в результирующий массив
     $words = preg_split("/([\s,'-.!?]+)/", $str, -1, PREG_SPLIT_DELIM_CAPTURE);
     $reversed = '';
+
+    //вывдем на экран полученный массив
     print_r($words);
 
+    // перебираем полученный массив поиндексно
     foreach ($words as $word) {
-        $reversed .= preg_replace_callback('/\p{L}+/u', function ($match) {
-            return reverseWordWithCasePreservation($match[0]);
-        }, $word);
+
+        // на всё суммарное совпадение последовательности алфавитных символов
+        // (любого языка) вызываем callback function,  в которой вызываем функцию
+        // поиска индексов символов в верхнем регистре и переворачивания слова
+
+        $reversed .= preg_replace_callback('/\p{L}+/u', function ($match) {return reverseWordWithCasePreservation($match[0]);}, $word);
     }
 
     return $reversed;
@@ -20,21 +27,25 @@ function reverseWordWithCasePreservation($word)
 {
     $length = strlen($word);
     $position = [];
-
+    //перебираем слово посимвольно, выясняем индекс символа в верхнем регистре
+    //индексы сохраняем в массив
     for ($i = 0; $i < $length; $i++) {
         if (ctype_upper($word[$i])) {
             $position[] = $i;
         }
     }
 
+    //во избежание путаницы переводим всё слово в нижний регистр
     $reversed = strtolower(strrev($word));
 
+    //перебираем массив с индексами, переводим символ с индексом в верхний регистр
     foreach ($position as $pos) {
         $reversed[$pos] = strtoupper($reversed[$pos]);
     }
 
     return $reversed;
 }
-$input = "heLLo, World!";
+
+$input = "heLLo-World!";
 $output = reverseWords($input);
 echo $output;
