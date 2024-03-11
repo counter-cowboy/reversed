d6c83655c4ff41f14a18b6e82d7d74af876b1506
counter-cowboy <79208562003@ya.ru>
2024-03-11 21:56:39 +0300

    docs: reverse word with no uppercase positioning

--- index.php
+++ index.php
@@ -2,10 +2,20 @@
 
 function reverseWords($str)
 {
-    $words=preg_split("([\s,.!?]+)", $str, -1, PREG_SPLIT_DELIM_CAPTURE);
-    $reversed='';
+    $words = preg_split("/([\s,'-.!?]+)/", $str, -1, PREG_SPLIT_DELIM_CAPTURE);
+    $reversed = '';
+    print_r($words);
 
     foreach ($words as $word) {
-        $reversed.=
+        $reversed .= preg_replace_callback('/\p{L}+/u', function ($match) {
+
+            print_r($match);
+            return strrev($match[0]);
+        }, $word);
     }
+    return $reversed;
 }
+
+$input = "third-part";
+$output = reverseWords($input);
+echo $output;
